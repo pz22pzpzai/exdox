5e479ed510a99795f92697389542caaf1fb1d971
pz22pzpzai <pz22pzpz@googlemail.com>
2026-09-21 21:09:10 +0100

    Add Facebook link to website footer

--- src/App.tsx
+++ src/App.tsx
@@ -10374,6 +10374,18 @@ function SiteFooterBlock() {
             <strong>Exdox</strong>
             <p>Receipt capture, review, claims, archive, and finance controls in one connected workflow.</p>
             <span>Mobile capture, web review, approvals, and CSV exports</span>
+            <a
+              className="site-footer-facebook"
+              href="https://www.facebook.com/profile.php?id=61594025846475"
+              target="_blank"
+              rel="noreferrer"
+              aria-label="Follow Exdox on Facebook"
+              title="Follow Exdox on Facebook"
+            >
+              <svg aria-hidden="true" viewBox="0 0 24 24" focusable="false">
+                <path d="M24 12.07C24 5.4 18.63 0 12 0S0 5.4 0 12.07C0 18.1 4.39 23.1 10.13 24v-8.44H7.08v-3.49h3.05V9.41c0-3.03 1.79-4.7 4.53-4.7 1.31 0 2.69.24 2.69.24v2.97h-1.51c-1.49 0-1.96.93-1.96 1.89v2.26h3.33l-.53 3.49h-2.8V24C19.61 23.1 24 18.1 24 12.07Z" />
+              </svg>
+            </a>
           </div>
           <div className="site-footer-links">
             <div>
